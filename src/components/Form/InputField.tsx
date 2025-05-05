86e179f7cb0ec9886245d22286884cc98f40ca66
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2025-05-05 13:29:28 +0300

    finish login ui&ux

--- src/components/Form/InputField.tsx
+++ src/components/Form/InputField.tsx
@@ -1,29 +1,36 @@
 import { CiSearch, CiUser } from "react-icons/ci";
 
 interface InputFieldProps {
-    id: string;
+    name: string;
     label: string;
     type: string;
     placeholder: string;
     value: string;
+    error?: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    leftIcon?: React.ReactNode;
+    rightIcon?: React.ReactNode;
+    className?: string;
+    disabled?: boolean;
 }
-const InputField = ({id, label, type, placeholder, value, onChange}: InputFieldProps) => {
+const InputField = ({name, label, type, placeholder, value, error, onChange, leftIcon, rightIcon, className, disabled}: InputFieldProps) => {
   return (
     <div className='mb-4'>
-        <label className='block mb-2 text-sm font-medium text-gray-700' htmlFor={id}>{label}</label>
-        <div className='group flex items-center justify-center gap-1 overflow-hidden px-4 py-1 border rounded-2xl border-neutral-100 focus-within:border-blue-500 transition'>
-            <CiSearch size={24} className="group-focus-within:hidden"/>
-            <input 
-                type={type}
-                id={id}
-                value={value}
-                onChange={onChange}
-                className='w-full p-2 border-0 active:border-0 focus:border-0 focus:outline-none'
-                placeholder={placeholder}
-                required />
-            <CiUser size={24} />
-        </div>
+      <label className='block mb-2 text-xl font-medium text-black' htmlFor={name}>{label}</label>
+      <div className={`group flex items-center justify-center gap-1 overflow-hidden px-4 py-1 border rounded-2xl border-neutral-100 focus-within:border-blue-500 transition text-black ${error ? 'border-red-500 focus-within:border-red-500 text-red-500' : ''}`}>
+        {rightIcon && rightIcon}
+        <input 
+          type={type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={`w-full p-2 text-xl border-0 active:border-0 focus:border-0 focus:outline-none ${className} ${disabled ? 'bg-gray-200' : ''}`}
+          disabled={disabled}
+          placeholder={placeholder} />
+        {leftIcon && leftIcon}
+      </div>
+      {error && <p className='mt-1 text-lg text-red-500'>{error}</p>}
     </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,99 @@
-import React from 'react'
-import InputField from '../components/Form/InputField'
-
+import { useState } from "react";
+import InputField from "../components/Form/InputField"
+import * as yup from "yup";
+import { loginSchema } from "../schemas/loginSchema"
+import { CiSearch, CiUser } from "react-icons/ci";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+
+    try {
+      // Validate the single field
+      const fieldSchema = yup.reach(loginSchema, name);
+      if (fieldSchema && 'validate' in fieldSchema) {
+        await fieldSchema.validate(value);
+      }
+      setErrors((prev) => ({ ...prev, [name]: "" }));
+
+      // Clear the error message if validation passes
+
+    } catch (err: any) {
+      setErrors((prev) => ({ ...prev, [name]: err.message }));
+    }
+  };
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await loginSchema.validate(user, { abortEarly: false });
+      console.log("Form submitted:", user);
+      // continue login logic here
+    } catch (err: any) {
+      const validationErrors: Record<string, string> = {};
+      err.inner.forEach((error: any) => {
+        validationErrors[error.path] = error.message;
+      });
+      setErrors(validationErrors);
+    }
+  };
+
   return (
     <div className='flex items-center justify-center h-screen'>
-        <div className='w-md p-6 bg-white rounded-2xl border'>
+        <div className='w-full max-w-lg min-h-[80vh] p-6 bg-white rounded-2xl border flex flex-col'>
             <h2 className='mb-1 text-[28px] font-bold'>الاداري لادارة عقود السيارات</h2>
             <p className='mb-6 text-2xl text-neutral-400'>يرجى تسجيل الدخول للمتابعة !</p>
-            <form>
-                <InputField label="اسم المستخدم" type="text" placeholder="ادخل اسم المستخدم" />
-                <div className='mb-4'>
-                    <label className='block mb-2 text-sm font-medium text-gray-700' htmlFor="password">كلمة السر</label>
-                    <input type="password" id="password" className='w-full p-2 border border-gray-300 rounded' placeholder='ادخل كلمة السر' required />
-                </div>
-                <button type="submit" className='w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600'>Login</button>
-            </form>
+            <form onSubmit={handleSubmit} className="flex flex-col justify-between flex-1 gap-4 ">
+              <div className="flex flex-col gap-4">
+                <InputField 
+                  label="اسم المستخدم" 
+                  type="text" 
+                  placeholder="ادخل اسم المستخدم" 
+                  name="email" 
+                  value={user.email} 
+                  error={errors.email}
+                  onChange={(e) => handleChange(e)}
+                  rightIcon={<CiSearch size={24} className="group-focus-within:hidden text-black"/>}
+                  leftIcon={<CiUser size={24} className="text-black"/>}
+                />
 
+                  <InputField
+                  label="كلمة السر"
+                  type={isPasswordVisible? "text" : "password"}
+                  placeholder="ادخل كلمة السر"
+                  name="password"
+                  value={user.password}
+                  error={errors.password}
+                  onChange={(e) => handleChange(e)}
+                  leftIcon={
+                    isPasswordVisible ?
+                      <LuEye size={24} className="text-black" onClick={togglePasswordVisibility} /> :
+                      <LuEyeClosed size={24} className="text-black" onClick={togglePasswordVisibility} />
+                  }
+                />
+              </div>
+              <button 
+                type="submit" 
+                className='w-full p-2 rounded-2xl text-2xl bg-primary-500 text-white hover:bg-primary-600 disabled:bg-neutral-100 disabled:text-neutral-400' 
+                disabled={ !( errors.email === '' && errors.password === '')? true: false}>
+                  Login
+                </button>
+            </form>
         </div>
-        
     </div>
   )
 }
